30342ca8d608f026a318a91cac046ba911b17da3
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-03-21 23:32:22 +0100

    Navigate via react router

--- index.jsx
+++ index.jsx
@@ -1,7 +1,29 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import {Link, Routes, Route, BrowserRouter} from 'react-router-dom';
+
+function FrontPage() {
+    return <div>
+        <h1>Kristiania Movie Database</h1>
+        <ul>
+            <li><Link to="/movies">List movies</Link></li>
+            <li><Link to="/movies/new">New Movie</Link></li>
+        </ul>
+    </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage/>}/>
+            <Route path="/movies/new" element={<h1>New Movie</h1>}/>
+            <Route path="/movies" element={<h1>List Movies</h1>}/>
+        </Routes>
+    </BrowserRouter>;
+}
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<h1>Kristiania Movie Database</h1>,
+ReactDOM.render(
+    <Application/>,
     document.getElementById("app")
 );

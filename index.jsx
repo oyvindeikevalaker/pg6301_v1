a9b933488aa5f94a749d3cae8c356c323194d38e
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-03-21 22:39:15 +0100

    Basic react app

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<h1>Kristiania Movie Database</h1>,
+    document.getElementById("app")
+);

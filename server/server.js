02a0bbb47f3b690e85dad7af23a066f556459a12
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-03-23 21:22:12 +0100

    Server React HTML built by PArcel

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import * as path from "path";
+
+const app = express();
+
+const MOVIES = [
+    {
+        title: "The Matrix",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        year: "1999"
+    },
+    {
+        title: "The Color Purple",
+        plot: "A black Southern woman struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: "1985"
+    }
+]
+
+app.get("/api/movies", (req, res) => {
+    res.json([MOVIES])
+})
+
+app.use(express.static(path.resolve("../dist")));
+
+const server = app.listen(3000, () => {
+    console.log("listening on http://localhost:" + server.address().port);
+});

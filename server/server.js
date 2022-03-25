4be0b66b922eaa9f1c63b9326f492c3a24dd2a61
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-03-25 20:43:14 +0100

    support heroku

--- server/server.js
+++ server/server.js
@@ -35,6 +35,6 @@ app.use((req, res) => {
     res.sendFile(path.resolve("..", "client", "dist", "index.html"));
 })
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log("listening on http://localhost:" + server.address().port);
 });
